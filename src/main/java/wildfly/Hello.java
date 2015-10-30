52d54902c5dbd9054d8c761ff54f502f7ec45567
Rodrigo Henrique Nonose <rodrigo.nonose@matera.com>
2015-10-30 16:29:00 -0200

    Add breaking test

---
+++ src/main/java/wildfly/Hello.java
@@ -0,0 +1,16 @@
+package wildfly;
+
+
+public class Hello {
+
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
---
+++ src/test/java/wildfly/HelloTest.java
@@ -0,0 +1,13 @@
+package wildfly;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(0, 1);
+    }
+
+}
